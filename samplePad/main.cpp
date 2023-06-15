986b954e529aed728d0974fb3415da4aa89c2832
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-06-15 14:12:55 +0900

    コントローラーサンプル追加

--- samplePad/main.cpp
+++ samplePad/main.cpp
@@ -5,6 +5,10 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	DINPUT_JOYSTATE input;
+	int i;
+	int Color;
+
 	// windowモード設定
 	ChangeWindowMode(Game::kWindowModo);
 	// ウインドウ名設定
@@ -27,7 +31,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		LONGLONG time = GetNowHiPerformanceCount();
 		// 画面のクリア
 		ClearDrawScreen();
+		// 入力状態を取得
+		GetJoypadDirectInputState(DX_INPUT_PAD1, &input);
 
+		// 画面に構造体の中身を描画
+		Color = GetColor(255, 255, 255);
+		DrawFormatString(0, 0, Color, "X:%d Y:%d Z:%d",
+			input.X, input.Y, input.Z);
+		DrawFormatString(0, 16, Color, "Rx:%d Ry:%d Rz:%d",
+			input.Rx, input.Ry, input.Rz);
+		DrawFormatString(0, 32, Color, "Slider 0:%d 1:%d",
+			input.Slider[0], input.Slider[1]);
+		DrawFormatString(0, 48, Color, "POV 0:%d 1:%d 2:%d 3:%d",
+			input.POV[0], input.POV[1],
+			input.POV[2], input.POV[3]);
+		DrawString(0, 64, "Button", Color);
+		for (i = 0; i < 32; i++)
+		{
+			DrawFormatString(64 + i % 8 * 64, 64 + i / 8 * 16, Color,
+				"%2d:%d", i, input.Buttons[i]);
+		}
 		// 裏画面を表画面を入れ替える
 		ScreenFlip();
 
